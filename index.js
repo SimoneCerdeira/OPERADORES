f2e170e1d85978bda3d2014596784ad735698b3e
simonecerdeira <simonescn.13@gmail.com>
2023-04-26 21:32:29 -0300

    exercicio aula 4

---
+++ index.js
@@ -0,0 +1,44 @@
+// const bool1 = true
+// const bool2 = false
+// const bool3 = !bool2
+
+// let resultado = bool1 && bool2
+// console.log("a. ", resultado)
+// resultado = bool1 && bool2 && bool3 
+// console.log("b. ", resultado) 
+// resultado = !resultado && (bool1 || bool2) 
+// console.log("c. ", resultado)
+// console.log("d. ", typeof resultado)
+
+
+
+// let primeiroNumero = Number(prompt("Digite um numero!"))
+// let segundoNumero = Number (prompt("Digite outro numero!"))
+// const soma = primeiroNumero + segundoNumero
+// console.log(soma)
+
+// const suaIdade = Number(prompt ("digite sua idade:"))
+// const idadeMelhorAmigo = Number(prompt ("digite a idade do seu melhor amigo:"))
+
+// console.log ("Sua idade é maior que a do seu amigo?")
+// console.log (suaIdade > idadeMelhorAmigo)
+// console.log (suaIdade - idadeMelhorAmigo)
+
+//const numeroPar = Number(prompt("digite um número PAR: "))
+//const resultado (numeroPar % 2===0)
+//console.log(" O numero é par?", resultado)
+//ao digitar numero PAR o resto sera sempre 0
+//ao digitar numero IMPAR o resto sera sempre 1
+
+// const idadeAnos = Number(prompt ("digite sua idade:"))
+// console.log ("Idade em meses: ", idadeAnos * 12)
+//console.log ("Idade em dias: ", idadeAnos * 365)
+//console.log ("Idade em horas: ", 365 * 24)
+
+const numero1 = Number (prompt("digite um numero:"))
+const numero2 = Number (prompt("digite outro numero:"))
+
+console.log("O primeiro numero é maior que o segundo?", numero1>numero2)
+console.log("O primeiro numero é igual ao segundo?", numero1===numero2)
+console.log("O primeiro numero é divisivel pelo segundo?", numero1%numero2===0)
+console.log("O segundo numero é divisivel pelo segundo?", numero2>numero1===0)
